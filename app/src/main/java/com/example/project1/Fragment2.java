5a44b18c5c0f88cb219a0c6c0156a4308e7eb393
gomaki <seungwoodev@gmail.com>
2021-07-01 16:28:02 +0900

    base updated

--- app/src/main/java/com/example/project1/Fragment2.java
+++ app/src/main/java/com/example/project1/Fragment2.java
@@ -1,65 +1,77 @@
 package com.example.project1;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
-////
+import android.content.Intent;
+import android.app.Activity;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.ImageView;
+
 public class Fragment2 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    MainActivity activity;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment2() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    // 프래그먼트가 액티비티로 올라오는 그 순간 -> onAttach 메소드
+    // the moment that fragment comes up to Activity -> onAttach method
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        activity = (MainActivity) getActivity();
     }
 
+    // 인플레이션은 OnCreateView에서 한다.
+    // inflation is at OnCreateView
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_2, container,false);
+
+        Button button1 = (Button) rootView.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.onImageChange(0);
+            }
+        });
+
+
+        Button button2 = (Button) rootView.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.onImageChange(1);
+            }
+        });
+
+
+        Button button3 = (Button) rootView.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.onImageChange(2);
+            }
+        });
+
+        // 프레그먼트에서 다른 뷰어 프레그먼트에 직접 접근은 못한다.
+        // 엑티비티쪽으로 요청 해야 한다.
+        // you do not have direct access to other viewer segments in the pree
+        // you have to request it to the Activity
+
+        return rootView;
     }
+
+
 }
--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.ListFragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    Fragment2 fragment1;
+    ViewerFragment fragment2;
+
+    FragmentManager manager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +36,18 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tab=findViewById(R.id.tab);
         tab.setupWithViewPager(vp);
 
-        ArrayList<Integer> images=new ArrayList<>();
-        images.add(R.drawable.profile);
-        images.add(R.drawable.gallery);
-        images.add(R.drawable.gallery);
+        manager = getSupportFragmentManager();
 
-        for(int i=0;i<3;i++) tab.getTabAt(i).setIcon(images.get(i));
+        fragment1 = (Fragment2) manager.findFragmentById(R.id.Fragment2);
+        fragment2 = (ViewerFragment) manager.findFragmentById(R.id.ViewerFragment);
+
+        Log.v(TAG, "Log.v 출력 " + TAG);
 
     }
+    public void onImageChange(int index) {
+        fragment2.setImage(index);
+    }
+
+
+
 }
