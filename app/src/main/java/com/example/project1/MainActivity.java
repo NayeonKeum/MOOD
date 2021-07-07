ff1c6a6c990fa214423760fc45abf3e7209c8e16
NayeonKeum <rmaskdus0208@gmail.com>
2021-07-07 12:48:47 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.project1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private final int Fragment_1 = 1;
+    private final int Fragment_2 = 2;
+    private final int Fragment_3 = 3;
+    private boolean PERMISSIONS_REQUEST_RESULT = false;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkSelfPermission();
+        if (PERMISSIONS_REQUEST_RESULT){
+            ViewPager vp=findViewById(R.id.viewpager);
+            VPAdapter adapter=new VPAdapter(getSupportFragmentManager());
+            vp.setAdapter(adapter);
+            // 연동
+            TabLayout tab=findViewById(R.id.tab);
+            tab.setupWithViewPager(vp);
+
+            ArrayList<Integer> images=new ArrayList<>();
+            images.add(R.drawable.profile);
+            images.add(R.drawable.gallery);
+            images.add(R.drawable.music);
+
+            for(int i=0;i<3;i++) tab.getTabAt(i).setIcon(images.get(i));
+        }
+    }
+    //권한에 대한 응답이 있을때 작동하는 함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //권한을 허용 했을 경우
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의
+                    Log.d("MainActivity", "권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+    }
+    public void checkSelfPermission() {
+        String temp = "";
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+        // 연락처 열람 권한
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_CONTACTS + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.ACCESS_FINE_LOCATION + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.ACCESS_COARSE_LOCATION + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.INTERNET + " ";
+        }
+        if (TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "),1);
+            PERMISSIONS_REQUEST_RESULT=true;
+        }else {
+            // 모두 허용 상태
+            PERMISSIONS_REQUEST_RESULT=true;
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+}
